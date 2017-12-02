0c183637b874219ee35f4b8abec1e368fddc787c
Merinaphing <33473496+Merinaphing@users.noreply.github.com>
2017-12-02 16:13:11 +0700

    test2

--- application/views/homeview.php
+++ application/views/homeview.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Hapread Online Bookstore</title>
 	<?php echo $js; // hai pen?>
-	<?php echo $css; ?>
+	<?php echo $css; //EPEN ?>
 </head>
 <body>
 	<?php echo $header; ?>
